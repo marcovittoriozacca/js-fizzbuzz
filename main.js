2832481b791fa8736224e5b2e87364e5a8859acd
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-09 14:59:30 +0100

    feat: style + dom elements

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
+const rowHTML = document.querySelector('.main-grid');
+
+
 //generazione dei numeri da 1 a 100 in console tramite ciclo for
 for(let i = 1; i <= 100; i++){
 
     if(i % 5 === 0 && i % 3 === 0){
-        console.log("FizzBuzz");
+        const squareBox = `<div class="bg-primary border border-3  border-dark  text-light shadow-lg d-flex justify-content-center align-items-center square">
+                        FizzBuzz
+                    </div>`;
+        rowHTML.innerHTML += squareBox;
 
     }else if (i % 3 === 0){
-        console.log("Fizz");
+        const squareBox = `<div class="bg-danger border border-1 border-dark shadow-lg d-flex justify-content-center align-items-center square">
+                        Fizz
+                    </div>`;
+        rowHTML.innerHTML += squareBox;        
 
     }else if (i % 5 === 0){
-        console.log("Buzz");
+        const squareBox = `<div class="bg-warning border border-1 border-dark shadow-lg d-flex justify-content-center align-items-center square">
+                                Buzz
+                        </div>`;
+        rowHTML.innerHTML += squareBox;
 
     }else{
-        console.log(i);
+        const squareBox = `<div class="bg-success border border-1 border-dark shadow-lg d-flex justify-content-center align-items-center square">
+                            ${i}
+                        </div>`;
+        rowHTML.innerHTML += squareBox;
         
     }
 }
